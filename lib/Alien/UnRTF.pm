1265a64feed7a1b662e2f9d7eb2df373d92392e9
Joenio Costa <joenio@joenio.me>
2016-06-11 04:31:36 -0300

    adding POD

--- lib/Alien/UnRTF.pm
+++ lib/Alien/UnRTF.pm
@@ -6,4 +6,29 @@ our $VERSION = '0.01';
 
 use parent 'Alien::Base';
 
+=head1 NAME
+
+Alien::UnRTF - Build and make available the unrtf tool
+
+=head1 DESCRIPTION
+
+This distribution installs UnRTF so that it can be used by other Perl
+distributions. If already installed for your operating system, and it can be
+found, this distribution will use the UnRTF that comes with your operating
+system, otherwise it will download it from the Internet, build and install it
+from you.
+
+=head1 AUTHOR
+
+Joenio Costa <joenio@joenio.me>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Joenio Costa.
+
+This is free software; you can redistribute it and/or modify it under the same
+terms as the Perl 5 programming language system itself.
+
+=cut
+
 1;
