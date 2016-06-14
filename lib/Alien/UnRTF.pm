97c46d60d249d43699be1766a498b5848e30aa25
Joenio Costa <joenio@joenio.me>
2016-06-14 02:03:51 -0300

    documenting how to use Alien::UnRTF

--- lib/Alien/UnRTF.pm
+++ lib/Alien/UnRTF.pm
@@ -10,6 +10,22 @@ use parent 'Alien::Base';
 
 Alien::UnRTF - Build and make available the unrtf tool
 
+=head1 SYNOPSIS
+
+From a Perl script
+
+  use Alien::UnRTF;
+  use Env qw( @PATH );
+  unshift @PATH, Alien::UnRTF->bin_dir;  # unrtf is now in your path
+
+From Alien::Base Build.PL
+
+  use Alien::Base::ModuleBuild;
+  my $builder = Module::Build->new(
+    alien_bin_requires => [ 'Alien::UnRTF' ],
+  );
+  $builder->create_build_script;
+
 =head1 DESCRIPTION
 
 B<**this is a beta release**>
@@ -19,6 +35,55 @@ it can be used by other Perl distributions. If already installed for your
 operating system, and it can be found, this distribution will use the UnRTF
 that comes with your operating system, otherwise it will download it from the
 Internet, build and install it from you.
+
+=head1 SEE ALSO
+
+=over
+
+=item *
+
+L<Alien>
+
+=item *
+
+L<Alien::Base>
+
+=back
+
+Similar modules:
+
+=over
+
+=item *
+
+L<Alien::qd>
+
+=item *
+
+L<Alien::flex>
+
+=item *
+
+L<Alien::m4>
+
+=item *
+
+L<Alien::bison>
+
+
+=item *
+
+L<Alien::CMake>
+
+=item *
+
+L<Alien::ffmpeg>
+
+=item *
+
+L<Alien::Gearman>
+
+=back
 
 =head1 AUTHOR
 
